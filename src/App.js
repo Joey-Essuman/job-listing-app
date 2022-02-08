57ec7790e0e0b53abb364d3bd151bce64ece6d17
Justice Cudjoe Essuman <justice.essuman@amalitech.org>
2022-02-08 18:23:21 +0000

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import data from "../src/data.json";
+import JobListing from "./components/JobListing";
+import Filters from "./components/Filters";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [jobs, setJobs] = useState(data);
+
+    const [activeFilters, setActiveFilters] = useState([]);
+    const filterJobs = terms => {
+        if (terms.length === 0) {
+            return;
+        }
+
+        const filteredJobs = data.filter(job => {
+            const tags = [...job.languages, ...job.tools, job.role, job.level];
+            return terms.every(term => tags.includes(term));
+        });
+        setJobs(filteredJobs);
+    };
+    const handleRemove = filter => {
+        if (activeFilters.length === 1) {
+            setActiveFilters([]);
+            setJobs(data);
+        } else {
+            setActiveFilters(prevState =>
+                prevState.filter(item => item !== filter)
+            );
+        }
+    };
+    const clearFilters = () => {
+        setActiveFilters([]);
+        setJobs(data);
+    };
+    const classes = {
+        main: "bg-header-desktop bg-cyan-verylight font-body text-base",
+        header: "h-40 w-full bg-cyan-default flex flex-column justify-center items-end",
+        jobs: "flex flex-col items-center gap-10 lg:gap-8 pt-32",
+    };
+
+    useEffect(() => {
+        filterJobs(activeFilters);
+    }, [activeFilters]);
+    return (
+        <div className={classes.main}>
+            <header
+                className={classes.header}
+                style={{
+                    backgroundImage: "url(/images/bg-header-desktop.svg",
+                    backgroundPosition: "top left",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                }}
+            >
+                {activeFilters.length !== 0 && (
+                    <Filters
+                        activeFilters={activeFilters}
+                        handleRemove={handleRemove}
+                        clearFilters={clearFilters}
+                    />
+                )}
+            </header>
+            <div className={classes.jobs}>
+                {jobs.map(job => (
+                    <JobListing
+                        job={job}
+                        filterJobs={filterJobs}
+                        setActiveFilters={setActiveFilters}
+                        activeFilters={activeFilters}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/JobListing.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+const JobListing = ({ job, setActiveFilters, activeFilters }) => {
+  const handleClick = (tag) => {
+    if (activeFilters.includes(tag)) {
+      return;
+    }
+    setActiveFilters((prevState) => [...prevState, tag]);
+  };
+  const {
+    company,
+    logo,
+    featured,
+    position,
+    role,
+    level,
+    postedAt,
+    contract,
+    location,
+    languages,
+    tools,
+  } = job;
+
+  const tags = [level, role, ...tools, ...languages];
+  const classes = {
+    wrapper:
+      "w-10/12 max-w-6xl flex flex-col lg:flex-row justify-between item-start lg:items-center gap-4 p-6 lg:p-9 rounded bg-white shadow-cyan",
+    info: "flex flex-col lg:flex-row items-start lg:items-center border-b lg:border-0 gap-6 pb-4 lg:pb-0",
+    image:
+      "w-12 lg:w-auto absolute lg:static transform -translate-y-12 lg:translate-y-0",
+    details: "flex gap-2 lg:gap-4 text-sm lg:text-base text-gray-400",
+    tags: "flex gap-4 flex-wrap",
+    name: "flex items-center text-sm font-bold text-cyan-default pt-4",
+    position: "text-cyan-verydark text-base lg:text-lg font-bold pt-2 pb-1",
+    tag: "bg-cyan-light hover:text-white hover:bg-cyan-default text-cyan-default px-2 pt-2 pb-1 rounded text-sm font-bold",
+    new: "bg-cyan-default text-white rounded-full px-2 pt-1.5 pb-0.5 text-xs uppercase font-normal ml-4 mr-2",
+    featuredFlag:
+      "bg-cyan-verydark text-white rounded-full px-2 pt-1.5 pb-0.5 text-xs uppercase font-normal",
+    featuredJob: "border-l-4 border-cyan-default pl-5 lg:pl-8",
+  };
+
+  return (
+    <div
+      className={`${classes.wrapper} ${featured ? classes.featuredJob : ""}`}
+    >
+      <div className={classes.info}>
+        <div className={classes.image}>
+          <img src={logo} alt={`${company} logo`} />
+        </div>
+
+        <div>
+          <div className={classes.name}>
+            <h2>{company}</h2>
+            {job.new && <span className={classes.new}>New!</span>}
+            {featured && <span className={classes.featuredFlag}>Featured</span>}
+          </div>
+
+          <h3 className={classes.position}>{position}</h3>
+          <ul className={classes.details}>
+            <li>{postedAt}</li>
+            <span aria-hidden="true">&#8226;</span>
+            <li>{contract}</li>
+            <span aria-hidden="true">&#8226;</span>
+            <li>{location}</li>
+          </ul>
+        </div>
+      </div>
+      <ul className={classes.tags}>
+        {tags.map((tag) => (
+          <li>
+            <button className={classes.tag} onClick={() => handleClick(tag)}>
+              {tag}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default JobListing;
